f78bbb4b6290a1fe7c01df4cca1ea2863ae0ff0a
Walter Restrepo <rpowalter@gmail.com>
2018-11-02 00:15:15 -0500

    Empty project

---
+++ server.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var path = require("path");
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+app.get('/', function (req, res) {
+    res.sendFile(
+        path.join(__dirname, '/src/views/index.html')
+    );
+});
+
+
+app.listen(3000, function () { console.log('Status On port 3000'); });
